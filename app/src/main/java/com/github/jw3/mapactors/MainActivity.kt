69bb59e9dc8a94295605b25eb54e927d6e9711ef
John Wass <jwass3@gmail.com>
2019-10-26 23:59:22 -0400

    sketch map

--- app/src/main/java/com/github/jw3/mapactors/MainActivity.kt
+++ app/src/main/java/com/github/jw3/mapactors/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.github.jw3.mapactors
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView.map = ArcGISMap(Basemap.Type.IMAGERY, 0.0, 0.0, 5)
+        val locationsLayer = GraphicsOverlay()
+        mapView.graphicsOverlays.add(locationsLayer)
     }
 }
